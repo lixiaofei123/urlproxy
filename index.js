21fcc086bc8366226679c3bd037e659a0c070bdf
lixiaofei123 <326256365@qq.com>
2024-04-26 19:40:19 +0000

    add basic auth

--- index.js
+++ index.js
@@ -14,15 +14,38 @@ app.use("/*", (req, resp, next) => {
     const baseUrl = req.baseUrl;
     if(baseUrl !== "" && baseUrl !== "/" && baseUrl !== "/auth/login"){
         if(PASSWORD !== ""){
-            const authentication = req.cookies["authentication"] || ""
-            jwt.verify(authentication,SIGN_KEY,(err, _decoded)=>{
-                if(err){
-                    resp.status(401).send(err.toString())
+            const authentication = req.cookies["Authentication"] || req.headers["authorization"]  || ""
+            if(authentication !== ""){
+                if(authentication.indexOf("Basic") === 0){
+                    const encodedstr = authentication.substring(6)
+                    const decodedstr = Buffer.from(encodedstr, 'base64').toString('ascii');
+                    const userpass = decodedstr.split(":")
+                    if(userpass.length === 2){
+                        const password = userpass[1]
+                        if(password === PASSWORD){
+                            next()
+                        }else{
+                            resp.status(401).send("wrong password")
+                        }
+                    }else{
+                        resp.status(400).send("unknown authorization type")
+                    }
+                }else if (authentication.indexOf("Bearer") === 0){
+                    let jwttoken =  authentication.substring(7)
+                    jwt.verify(jwttoken,SIGN_KEY,(err, _decoded)=>{
+                        if(err){
+                            resp.status(401).send(err.toString())
+                        }else{
+                            next()
+                        }
+                    })
                 }else{
-                    next()
+                    resp.status(400).send("unknown authorization type")
                 }
-            })
-            
+                
+            }else{
+                resp.status(401).send("authorization is empty")
+            }
         }else{
             next()
         }
